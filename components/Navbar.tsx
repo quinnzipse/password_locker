70cc2110b4bd5e727631b19dbe178d9f36b95989
Paul Schultz <tanksrule1015@gmail.com>
2021-05-18 20:32:52 -0500

    Changed Navbar to basic

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,18 +9,7 @@ const Navbar = () => {
   }
 
   return (
-    <nav className='text-white bg-blue-500 h-16 p-3 flex items-center justify-end rounded'>
-      <ul className='flex justify-center items-center list-none mx-2 my-6'>
-        <li className='mx-2 my-6'>
-          <Link href='/'>
-            <a className='lg:inline-flex lg:w-auto w-full rounded bg-gray-700 px-3 py-2'>Test</a>
-          </Link>
-        </li>
-        <li className='mx-2 my-6'>Test2</li>
-        <li className='mx-2 my-6'>Test3</li>
-        <li className='mx-2 my-6'>Test4</li>
-      </ul>
-    </nav>
+    <nav className='text-white bg-blue-500 p-3 text-center font-semibold text-xl'>Password Locker</nav>
   )
 }
 
