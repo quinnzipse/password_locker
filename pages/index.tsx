45d3938f128d841fd651d3a2a8237b9aaab4af76
Quinn Zipse <qzipse@outlook.com>
2021-05-23 15:18:35 -0500

    Playing around with tailwind for sign in form
    Signed-off-by: Quinn Zipse <qzipse@outlook.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,34 +1,42 @@
 function Home () {
   return (
     <>
-      <div className="container mx-auto h-96 my-10 p-10 bg-indigo-200 rounded-xl shadow-xl">
-        <div className="grid-cols-1 rounded-md p-5">
-          <h1 className="text-3xl mb-0">Sign In</h1>
-          <form className="p-3 pl-0">
-            <div className="mt-5">
-              <label htmlFor="username" className="pb-2 block text-lg">
-                Username
-              </label>
-              <input
-                type="text"
-                name="username"
-                className="rounded py-1 px-2"
-              ></input>
+      <div className="container mx-auto">
+        <div className="flex justify-end p-4">
+          <div className="w-full md:w-1/2 lg:w-1/3 xl:w-1/4">
+            <div
+              style={{ zIndex: -1 }}
+              className="inline-flex w-100 transform rotate-45 bg-red-400"
+            ></div>
+            <div className="p-10  bg-gray-200 shadow-md">
+              <form action="login">
+                <h1 className="text-3xl mb-0">Sign In</h1>
+                <div className="mt-5">
+                  <label htmlFor="username" className="pb-2 block text-lg">
+                    Username
+                  </label>
+                  <input
+                    type="text"
+                    name="username"
+                    className="rounded py-1 px-2 w-full"
+                  ></input>
+                </div>
+                <div className="mt-5">
+                  <label htmlFor="password" className="pb-2 block text-lg">
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    id="password"
+                    className="rounded py-1 px-2 w-full"
+                  ></input>
+                </div>
+                <button className="rounded transition bg-indigo-200 hover:bg-indigo-500 hover:text-indigo-100 shadow-md text-indigo-800 py-1 px-3 mt-6">
+                  Sign In
+                </button>
+              </form>
             </div>
-            <div className="mt-5">
-              <label htmlFor="password" className="pb-2 block text-lg">
-                Password
-              </label>
-              <input
-                type="password"
-                id="password"
-                className="rounded py-1 px-2"
-              ></input>
-            </div>
-            <button className="rounded bg-blue-500 py-1 px-3 mt-6 text-white">
-              Sign In
-            </button>
-          </form>
+          </div>
         </div>
       </div>
     </>
